38cfbd39e0f53695119d168663c1c767222196bb
Charan Vaddepally <charan316.cv@gmail.com>
2026-06-25 16:38:35 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    private boolean canEat(int piles[], int k, int h) {
+        int n = piles.length;
+        int hours = 0;
+        for(int i=0; i<n; i++) {
+            hours += (piles[i]/k + (piles[i]%k==0? 0: 1));
+        }
+
+        return hours <= h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 1000000000;
+        int ans = Integer.MAX_VALUE;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(canEat(piles, mid, h)) {
+                high = mid - 1;
+                ans = Math.min(ans, mid);
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+}
